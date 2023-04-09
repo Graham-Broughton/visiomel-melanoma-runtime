9706c5e528937a0bebc696be24e1dd0efc066098
Graham-Broughton <broughton.graham@gmail.com>
2023-04-09 01:04:45 +0000

    updated

--- src/tfrecord.py
+++ src/tfrecord.py
@@ -1,14 +1,16 @@
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-import pyvips as pv
-from loguru import logger
-from pathlib import Path
 import os
 import subprocess
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import pyvips as pv
+import tensorflow as tf
+
+#from loguru import logger
 from sklearn.preprocessing import OneHotEncoder
 
-logger.remove(0)
+#logger.remove(0)
 # logger.add()
 
 TFRECORD_IMAGE_RATIO = {
@@ -18,12 +20,18 @@ TFRECORD_IMAGE_RATIO = {
     4: 6
 }
 
+class CorruptDownload(Exception):
+    pass
+
+
 class MakeTFRecords:
     def __init__(self, df_path, page):
+        self.page = page
         self.data_path = Path(df_path)
         if not os.path.exists(self.data_path / 'tfrecords'):
             os.makedirs(self.data_path / 'tfrecords')
-        
+            self.record_path = Path(self.data_path / 'tfrecords')
+
         train_meta = pd.read_csv(self.data_path / 'train_metadata.csv')
         train_labels = pd.read_csv(self.data_path / 'train_labels.csv')
         self.train = train_meta.merge(train_labels, on='filename')
@@ -35,7 +43,7 @@ class MakeTFRecords:
 
     def preprocess_feats_2_encode(self):
         feats = self.train.copy()
-        
+
         enc = OneHotEncoder(drop="first", sparse_output=False)
         enc.fit(np.array(feats["melanoma_history"]).reshape(-1, 1))
 
@@ -43,7 +51,7 @@ class MakeTFRecords:
 
         feats['sex'] = feats['sex'].replace({1: 0}).replace({2: 1}).astype(int)
 
-        return pd.concat([
+        feats = pd.concat([
             feats[['age', 'sex']],
             pd.DataFrame(
                 enc.transform(np.array(feats["melanoma_history"]).reshape(-1, 1)),
@@ -51,9 +59,26 @@ class MakeTFRecords:
                 index=feats.index,
             ).astype(int),
             feats['relapse'].astype(int)
-        ])
+        ], axis=1)
 
+        files = self.train[['filename', 'tif_cksum', 'tif_size', 'us_tif_url', 'eu_tif_url', 'as_tif_url']]
+        return feats, files
 
+    def dl_image(self, url, filename):
+        subprocess.run(['s5cmd', '--no-sign-request', 'cp', url, '.'], check=True)
+        output = subprocess.run(['cksum', filename], capture_output=True, check=True)
+        cksum, filesize, _ = output.stdout.decode().split()
+        return cksum, filesize
+
+    def verify_dl(self, file_row, cksum, filesize):
+        if file_row['tif_cksum'] != cksum or file_row['tif_size'] != filesize:
+            raise CorruptDownload(f"Corrupt download: {file_row['filename']}")
+
+    def create_example(self, image, feats_row, height, width):
+        image_raw = image.write_to_memory()
+        
+    def main(self):
+        
     @staticmethod
     def image_feature(value):
         """Returns a bytes_list from a string / byte."""
@@ -67,10 +92,3 @@ class MakeTFRecords:
         return tf.train.Feature(
             int64_list=tf.train.Int64List(value=[value])
         )
-
-
-
-
-
-def prelim(page):
-    
