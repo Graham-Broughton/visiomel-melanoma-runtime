0a26b8d289f8d21554ddd22c5d7a95df93225428
Graham-Broughton <broughton.graham@gmail.com>
2023-04-26 03:28:04 -0700

    update

---
+++ submission/src/data/transform.py
@@ -0,0 +1,41 @@
+from torchvision import transforms
+from data.dataset_helper import reverse_norm
+
+
+def get_transformation(patch_size=224, mean=None, std=None):
+    '''
+    Get data augmentation for different dataset
+    patch_size: size of the crops
+    mean: sample mean, default [0.5,0.5,0.5] if not defined
+    std: sample std, default [0.25, 0.25, 0.25] if not defined
+    '''
+    if mean is None:
+        mean = [0.5,0.5,0.5]
+    if std is None:
+        std = [0.25, 0.25, 0.25]
+    return {
+        'train': transforms.Compose(
+            [
+                transforms.ToPILImage(),
+                transforms.RandomCrop(patch_size),
+                transforms.ColorJitter(
+                    brightness=0.35, contrast=0.5, saturation=0.1, hue=0.16
+                ),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        ),
+        'val': transforms.Compose(
+            [
+                transforms.RandomCrop(patch_size),
+                transforms.Normalize(mean, std),
+            ]
+        ),
+        'predict': transforms.Compose([transforms.Normalize(mean, std)]),
+        'normalize': transforms.Compose([transforms.Normalize(mean, std)]),
+        'unnormalize': transforms.Compose(
+            [transforms.Normalize(*reverse_norm(mean, std))]
+        ),
+    }
